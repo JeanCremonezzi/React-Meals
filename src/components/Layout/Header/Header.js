8901b3da0db87e281732fa61e81b5d4095680d82
JeanCremonezzi <jeancremonezzi@hotmail.com>
2022-02-06 15:45:36 -0300

    feat: Header component

---
+++ src/components/Layout/Header/Header.js
@@ -0,0 +1,18 @@
+import styles from "./Header.module.css";
+
+import MealsImg from "../../../assets/meals.jpg";
+
+export const Header = () => {
+	return (
+		<>
+			<header className={styles.header}>
+				<h1>React Meals</h1>
+				<button>Cart</button>
+			</header>
+
+			<div className={styles["main-image"]}>
+				<img src={MealsImg} alt="A table full of delicious food!" />
+			</div>
+		</>
+	);
+};
