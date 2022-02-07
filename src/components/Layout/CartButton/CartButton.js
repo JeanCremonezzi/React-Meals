f446955cfd90dc1dd8eaeadae162d667e75c95f7
JeanCremonezzi <jeancremonezzi@hotmail.com>
2022-02-07 05:14:08 -0300

    feat: Using useEffect to animate Cart Button when an item is added to cart

--- src/components/Layout/CartButton/CartButton.js
+++ src/components/Layout/CartButton/CartButton.js
@@ -1,4 +1,4 @@
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 
 import CartContext from "../../../store/cart-context";
 import { CartIcon } from "../../Cart/CartIcon/CartIcon";
@@ -6,14 +6,34 @@ import { CartIcon } from "../../Cart/CartIcon/CartIcon";
 import styles from "./CartButton.module.css";
 
 export const CartButton = (props) => {
+	const [btnHighlighted, setBtnHighlighted] = useState(false);
+
 	const ctxCart = useContext(CartContext);
 
 	const amountCartItems = ctxCart.items.reduce((current, item) => {
 		return current + item.amount;
 	}, 0);
 
+	const btnClasses = `${styles.button} ${btnHighlighted ? styles.bump : ""}`;
+
+	useEffect(() => {
+		if (ctxCart.items.length === 0) {
+			return;
+		}
+
+		setBtnHighlighted(true);
+
+		const identifier = setTimeout(() => {
+			setBtnHighlighted(false);
+		}, 300);
+
+		return () => {
+			clearTimeout(identifier);
+		};
+	}, [ctxCart.items]);
+
 	return (
-		<button className={styles.button} onClick={props.onClick}>
+		<button className={btnClasses} onClick={props.onClick}>
 			<span className={styles.icon}>
 				<CartIcon />
 			</span>
